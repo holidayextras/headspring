00ccd8bed525fe6b33d768dbce5ef5208f0b2fe9
doomcrata <nick.kypreos@socrata.com>
2015-11-23 21:15:03 -0800

    some tool clean up and refactoring of app

--- headspring/headspring.py
+++ headspring/headspring.py
@@ -1,75 +1,38 @@
-import base64
-import logging
-import httplib2
+"""
+Simplt Flask app that acts as a reasonably generic endpoint for
+posting to Google pubsub.
+
+Configuration is controlled by the "config" file
+run from the same directory as the app.
+
+"""
+
 import hashlib
 import json
-import os
-import sys
-import traceback
-import uuid
 
-from oauth2client.client import GoogleCredentials
-from apiclient import discovery
 from flask import Flask, abort, jsonify, request
 
-from tools import generate_id
-from ConfigParser import ConfigParser
+from tools import generate_id, get_config_parser, get_pubsub_client, publish
 
+app = Flask(__name__)
 
-os.chdir(os.path.dirname(sys.argv[0]))  # change into execution directory
-config = ConfigParser()
-
-try:
-    with open('config') as _fp:
-        config.readfp(_fp)
-except Exception as e:
-    logging.info('failed to open config file')
-
+config = get_config_parser('config', app.logger)
 
 PROJ_NAME = config.get('override', 'proj_name')
 
-app = Flask(__name__)
-def get_pubsub_client(version='v1beta2'):
-    """Build the pubsub client."""
 
-    PUBSUB_SCOPES = ['https://www.googleapis.com/auth/pubsub']
-    get_pubsub_client.PUBSUB_SCOPES = ['https://www.googleapis.com/auth/pubsub']
-    credentials = GoogleCredentials.get_application_default()
-
-    if credentials.create_scoped_required():
-        credentials = credentials.create_scoped(get_pubsub_client.PUBSUB_SCOPES)
-
-    http = httplib2.Http()
-    credentials.authorize(http)
-
-    return discovery.build('pubsub', version, http=http)
-
-
-def publish(client, pubsub_topic, data_line, msg_attributes=None):
-    """Publish to the given pubsub topic."""
-    data = base64.b64encode(data_line)
-    msg_payload = {'data': data}
-    if msg_attributes:
-        msg_payload['attributes'] = msg_attributes
-    body = {'messages': [msg_payload]}
-    resp = client.projects().topics().publish(
-        topic=pubsub_topic, body=body).execute(num_retries=config.getint('override', 'num_retries'))
-    logging.info(resp)
-    return resp
-
-#
-#
-#
 @app.route('/')
 def index():
     """Generic just because"""
     return 'producer!'
+
 
 @app.route(config.get('override', 'health_route'))
 def ping():
     """health"""
     app.logger.info('health check')
     return 'pong'
+
 
 @app.route(config.get('override', 'post_route'), methods=['POST'])
 def producer():
@@ -99,17 +62,20 @@ def producer():
 
     try:
         app.logger.debug('writing to stream')
-        publish(client, config.get('override', 'stream_name'), json.dumps(reqdat_))
-#        k.put_record(stream_name_, json.dumps(reqdat_), hsh)
+        publish(client,
+                config.get('override', 'stream_name'),
+                json.dumps(reqdat_),
+                app.logger,
+                num_retries=config.getint('override', 'num_retries'))
     except Exception as e:
         app.logger.error(e)
         app.logger.error('failed to put to stream ' + json.dumps(reqdat_))
         abort(503, 'Internal Error')
         return json.dumps({'response': 'failure'})
-        
+
     return json.dumps({'response': 'success'})
 
 
 if __name__ == '__main__':
-    
+
     app.run(debug=True, port=5001)
--- headspring/tools.py
+++ headspring/tools.py
@@ -1,9 +1,51 @@
+"""
+Tools for running the headspring app.
+
+includes:
+
+get_config_parser: for parsing config file for app
+
+app_dir_switch: for moving into app directory
+
+generate_id: for generating a timestamp and urn
+
+get_pubsub_client: for getting the Google pubsub client
+
+publish: publishing to pubsub
+"""
+
+import base64
 import datetime
-import uuid
 import httplib2
+import os
+import sys
+import uuid
 from apiclient import discovery
 
+from ConfigParser import ConfigParser
 from oauth2client.client import GoogleCredentials
+
+
+def get_config_parser(filename, logging):
+    """Open logging file"""
+
+    config = ConfigParser()
+
+    try:
+        with open(filename) as _fp:
+            config.readfp(_fp)
+    except Exception:
+        logging.info('failed to open config file')
+        raise Exception('failed to open config file')
+
+    return config
+
+
+def app_dir_switch():
+    """Switch into working directory"""
+
+    os.chdir(os.path.dirname(sys.argv[0]))  # change into execution directory
+
 
 def generate_id():
     """Generate an ID and timestamp"""
@@ -27,7 +69,7 @@ def get_pubsub_client(version='v1beta2'):
     return discovery.build('pubsub', version, http=http)
 
 
-def publish(client, pubsub_topic, data_line, msg_attributes=None):
+def publish(client, pubsub_topic, data_line, logging, num_retries=3, msg_attributes=None):
     """Publish to the given pubsub topic."""
     data = base64.b64encode(data_line)
     msg_payload = {'data': data}
@@ -35,11 +77,11 @@ def publish(client, pubsub_topic, data_line, msg_attributes=None):
         msg_payload['attributes'] = msg_attributes
     body = {'messages': [msg_payload]}
     resp = client.projects().topics().publish(
-        topic=pubsub_topic, body=body).execute(num_retries=NUM_RETRIES)
+        topic=pubsub_topic, body=body).execute(num_retries=num_retries)
+    logging.info(resp)
     return resp
-
-
-if __name__ == '__main__':
-    print generate_id()
-    client = get_pubsub_client()
-    print client
+#
+#if __name__ == '__main__':
+#    print generate_id()
+#    client = get_pubsub_client()
+#    print client
