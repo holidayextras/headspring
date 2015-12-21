17b9d1e3ae02591a8f262eef7d13ec636972fff8
pat <pat@22acacia.com>
2015-12-20 22:40:42 -0800

    eq, not ne

--- build_and_push_docker_image.sh
+++ build_and_push_docker_image.sh
@@ -4,13 +4,13 @@
 docker build -t acacia acacia/
 ret_val=$?
 
-if [ $ret_val -ne 0 ]; then
+if [ $ret_val -eq 0 ]; then
   #  build the application image
   docker build -t gcr.io/hx-test/source-master-cidev .
   ret_val=$?
 fi
 
-if [ $ret_val -ne 0 ]; then
+if [ $ret_val -eq 0 ]; then
   #  push the application image to gcr
   /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/hx-test/source-master-cidev
   ret_val=$?
