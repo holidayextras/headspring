3e7ae2c8f8c9b0d1a1cd7e16ef96eba549bdab1d
pat <pat@22acacia.com>
2015-12-21 22:54:35 -0800

    make it look the same as yellow sub

--- build_and_push_docker_image.sh
+++ build_and_push_docker_image.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-#  build the base image
-docker build -t acacia acacia/
-ret_val=$?
+/opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file account.json
+ret_var=?
+if [ $ret_var -eq 0 ]; then
+  service docker start
+  ret_var=$?
+fi
+
+if [ $ret_var -eq 0 ]; then
+  #  build the base image
+  docker build -t acacia acacia/
+  ret_val=$?
+fi
 
 if [ $ret_val -eq 0 ]; then
   #  build the application image
-  docker build -t gcr.io/hx-test/source-master-cidev .
+  docker build -t gcr.io/hx-test/source-master .
   ret_val=$?
 fi
 
 if [ $ret_val -eq 0 ]; then
   #  push the application image to gcr
-  /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/hx-test/source-master-cidev
+  /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/hx-test/source-master
   ret_val=$?
 fi 
 
