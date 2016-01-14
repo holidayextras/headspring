738304da0e6a5323f6ad4955eb073e2610d93dc4
doomsuckle <nickkypreos@gmail.com>
2016-01-13 23:00:42 -0800

    put in wsgi container

--- headspring/start.py
+++ headspring/start.py
@@ -3,7 +3,7 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from headspring import app
 
-server = HTTPServer(app)
+server = HTTPServer(WSGIContainer(app))
 server.bind(8080)
 server.start(0)  # Forks multiple sub-processes
 IOLoop.current().start()
